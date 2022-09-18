788bbfe5bc6c37748a33228ef6ef54b51d07d5d0
JP3015 <RM87894@fiap.com.br>
2022-09-17 21:01:19 -0300

    Application Test

--- src/test/java/br/com/fiap/challenge/ChallengeApplicationTests.java
+++ src/test/java/br/com/fiap/challenge/ChallengeApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.fiap.esporte;
+package br.com.fiap.challenge;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ChallengeApplication.class)
 public class ChallengeApplicationTests {
 
     @Test
